a74f188b2fa01dcdfdfe99b66a2469041c6292ad
quangthanh72 <thanhquangk22@gmail.com>
2023-05-02 11:28:33 +0700

    Add story

--- components/home/Stories.js
+++ components/home/Stories.js
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import { USERS } from '../../data/Users'
 
 const Stories = () => {
   return (
-    <View>
-      <Text style={{color: 'black'}}>Stories</Text>
+    <View style={{marginBottom:13}}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {USERS.map((story, index) => (
+        <Image source={{ur: story.image}}
+        style={styles.story}
+        />
+        ))}
+        
+      </ScrollView>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  story:{
+
+  }
+})
 export default Stories
---
+++ data/Users.js
@@ -0,0 +1,22 @@
+export const USERS = [
+    {
+        user: 'bơ',
+        image:'https://i.pinimg.com/originals/01/17/20/011720703d78d60fccf9f79821e14159.jpg'
+    },
+    {
+        user: 'chuối',
+        image: 'https://i.pinimg.com/originals/fa/e4/df/fae4dfd8358ed86dc98a21e1668724d4.jpg'
+    },
+    {
+        user: 'dưa hấu',
+        image: 'https://i.pinimg.com/originals/fa/e4/df/fae4dfd8358ed86dc98a21e1668724d4.jpg'
+    },
+    {
+        user: 'sầu riêng',
+        image: 'https://i.pinimg.com/originals/fa/e4/df/fae4dfd8358ed86dc98a21e1668724d4.jpg'
+    },
+    {
+        user: 'xoài',
+        image: 'https://i.pinimg.com/originals/fa/e4/df/fae4dfd8358ed86dc98a21e1668724d4.jpg'
+    },
+]
