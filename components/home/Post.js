0ca11bb635d9feb13ff931a0e3a9e63dd36143c6
quangthanh72 <thanhquangk22@gmail.com>
2023-05-03 21:30:38 +0700

    Update Post.js

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,4 +1,4 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import React from 'react'
 import { Divider } from 'react-native-elements'
 const Post = ({post}) => {
@@ -12,14 +12,30 @@ const Post = ({post}) => {
 }
 
 const PostHeader =({post}) =>{
-  <View>
-    <View>
-      <Image/>
-      <Text></Text>
-    </View>
+  <View 
+    style={{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      margin:5,
+      alignItems: 'center',
+      }} 
+  >
+      <View>
+        <Image source={{ uri: post.profile_picture }} style={styles.story} />
+        <Text></Text>
+      </View>
   </View>
 }
-
+const styles = StyleSheet.create({
+  story:{
+    width:35,
+    height:35,
+    borderRadius:50,
+    borderWidth:1.6,
+    marginLeft:6,
+    borderColor: '#ff8501'
+  },
+})
 
 
 export default Post
