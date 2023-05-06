ab53134abd814dcfb1623018f534532876455e41
quangthanh72 <thanhquangk22@gmail.com>
2023-05-06 11:17:27 +0700

    Update Post (Footer)

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import { Divider } from "react-native-elements";
 
@@ -7,7 +7,12 @@ const Post = ({ post }) => {
     <View style={{ marginBottom: 30 }}>
       <Divider width={1} orientation="vertical" />
       <PostHeader post={post} />
-      
+      <View style={{ marginHorizontal: 10, marginTop: 10 }}>
+        <PostFooter />
+        <Likes post={post} />
+        <Caption post={post} />
+        <CommentSection post={post}/>
+      </View>
     </View>
   );
 };
@@ -15,28 +20,99 @@ const Post = ({ post }) => {
 const PostHeader = ({ post }) => {
   return (
     <View>
-    <View
-      style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-        margin: 5,
-        alignItems: "center",
-      }}
-    >
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Image source={{ uri: post.profile_picture }} style={styles.story} />
-        <Text style={{ color: "black", marginLeft: 5, fontWeight: "700" }}>
-          {post.user}
-        </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          margin: 5,
+          alignItems: "center",
+        }}
+      >
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image source={{ uri: post.profile_picture }} style={styles.story} />
+          <Text style={{ color: "black", marginLeft: 5, fontWeight: "700" }}>
+            {post.user}
+          </Text>
+        </View>
+        <Text style={{ fontWeight: "900" }}>...</Text>
       </View>
-      <Text style={{ fontWeight: "900" }}>...</Text>
+      <View style={{ width: "100%", height: 450 }}>
+        <Image
+          source={{ uri: post.imageUrl }}
+          style={{ height: "100%", resizeMode: "cover" }}
+        />
+      </View>
     </View>
-    <View style={{width:'100%',height:450}}>
-      <Image source={{ uri: post.imageUrl}} style={{height:'100%',resizeMode: 'cover'}} />
-    </View>
-    </View> 
   );
 };
+
+const postFooterIcon = [
+  {
+    name: "Like",
+    imageUrl: "https://img.icons8.com/?size=512&id=86721&format=png",
+    likeImageUrl: "https://img.icons8.com/?size=512&id=Mjt9Tkm04cgv&format=png",
+  },
+  {
+    name: "Comment",
+    imageUrl: "https://img.icons8.com/?size=512&id=93386&format=png",
+  },
+  {
+    name: "Share",
+    imageUrl: "https://img.icons8.com/?size=512&id=yXLO0XxrYU1U&format=png",
+  },
+  {
+    name: "Save",
+    imageUrl: "https://img.icons8.com/?size=512&id=82461&format=png",
+    saveImageUrl: "https://img.icons8.com/?size=512&id=26089&format=png",
+  },
+];
+
+const PostFooter = () => (
+  <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+    <View style={styles.leftFooterIconsContainer}>
+      <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcon[0].imageUrl} />
+      <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcon[1].imageUrl} />
+      <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcon[2].imageUrl} />
+    </View>
+
+    <View>
+      <Icon imgStyle={styles.footerIcon} imgUrl={postFooterIcon[3].imageUrl} />
+    </View>
+  </View>
+);
+
+const Icon = ({ imgStyle, imgUrl }) => (
+  <TouchableOpacity>
+    <Image style={imgStyle} source={{ uri: imgUrl }} />
+  </TouchableOpacity>
+);
+
+const Likes = ({ post }) => (
+  <View style={{flexDirection:'row', marginTop:4}}>
+    <Text style={{ fontWeight: "600" }}>
+      {" "}
+      {post.likes.toLocaleString("en")} likes{" "}
+    </Text>
+  </View>
+);
+const Caption = ({post}) => (
+  <View style={{  marginTop: 5}}> 
+    <Text>
+    <Text style={{fontWeight: '600'}}>
+        {post.user} 
+    </Text>
+    <Text> {post.caption}</Text>
+    </Text>
+  </View>
+);
+const CommentSection = ({post}) => (
+  <Text style={{ color: 'grey'}}>
+    View{post.comments.length > 1 ? ' all ' : ' '} {post.comments.length}{" "}
+    {post.comments.length > 1 ? 'comments' : 'comment'}
+  </Text>
+)
+
+
 
 
 const styles = StyleSheet.create({
@@ -47,6 +123,15 @@ const styles = StyleSheet.create({
     borderWidth: 1.6,
     marginLeft: 6,
     borderColor: "#ff8501",
+  },
+  footerIcon: {
+    width: 33,
+    height: 33,
+  },
+  leftFooterIconsContainer: {
+    flexDirection: "row",
+    width: "32%",
+    justifyContent: "space-between",
   },
 });
 
--- data/posts.js
+++ data/posts.js
@@ -5,7 +5,7 @@ export const POSTS=[
     imageUrl: 'https://wizarddojo.files.wordpress.com/2015/06/23.jpg',
     user: USERS[0].user,
     likes: 7870,
-    caption: 'How\' moving castle',
+    caption: 'How\' moving castle  \nLâu đài bay của pháp sư Howl, hay Lâu đài di động của Howl, là một bộ phim điện ảnh hoạt hình Nhật Bản thuộc thể loại kỳ ảo năm 2004 do Miyazaki Hayao đạo diễn kiêm viết kịch bản, dựa trên tiểu thuyết cùng tên năm 1986 của nữ tác giả người Anh Diana Wynne Jones.',
     profile_picture: USERS[0].image,
     comments:[
         {
@@ -22,7 +22,7 @@ export const POSTS=[
 {
     imageUrl: 'https://pic-bstarstatic.akamaized.net/ugc/3c8c48f2dadff8351ae4d73150cec086f5d1bb41.jpg',
     user: USERS[1].user,
-    likes: 7870,
+    likes: 7872,
     caption: 'Harry Potter bủh',
     profile_picture: USERS[1].image,
     comments:[
@@ -30,10 +30,7 @@ export const POSTS=[
             user:'bơ',
             comment: 'Cool'
         },
-        {
-            user:'dưa hấu',
-            comment: 'Love it'    
-        }
+   
     ]
     
 }
